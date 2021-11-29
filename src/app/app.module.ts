c354720530c781d3a19c8a9b0901f337dee35238
Patrick Prakash <patrickprakash8@gmail.com>
2021-11-29 23:03:35 +0530

    Added import on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Feature Modules
+import { AuthModule } from './auth/auth.module';
+
+//Shared Module
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
