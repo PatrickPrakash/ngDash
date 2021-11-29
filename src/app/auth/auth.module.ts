2904c921a061df53fbd7d24d879b8294768b08b1
Patrick Prakash <patrickprakash8@gmail.com>
2021-11-29 21:36:31 +0530

    Renamed the module to auth module -- Mistaken for dashboard Module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+
+@NgModule({
+  declarations: [
+    SignupComponent,
+    SigninComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    SigninComponent,
+    SignupComponent
+  ]
+})
+export class AuthModule { }
