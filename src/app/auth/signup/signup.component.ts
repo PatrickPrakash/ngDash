3e5b985fef296853e87f3cf4f1659e1167132c05
Patrick Prakash <patrickprakash8@gmail.com>
2021-11-30 21:04:51 +0530

    HttpClient Integration in auth components

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { HttpService } from '../http.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { AuthModel } from '../auth-model.Auth';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -15,11 +18,34 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('',[Validators.required, Validators.minLength(5)]);
 
-  constructor() { }
+  authModel :  AuthModel = {
+    email: '',
+    password: ''
+  };
+;
+  constructor(private _snackBar: MatSnackBar, private httpService: HttpService) { }
+
+  emailHolder = new FormControl('', [Validators.required, Validators.email]);
+  passwordHolder = new FormControl('',[Validators.required, Validators.minLength(5)]);
 
   matcher = new MyErrorStateMatcher();
+
+ 
+
+  Submit(): void {
+    // this.email.invalid ? this.openSnackBar('Email you entered is invalid') : this.password.invalid  ? this.openSnackBar('Password you entered is invalid') :  this.email.invalid && this.password.invalid ? this.openSnackBar('Credentials Invalid') : this.httpService.signUp(this.email.value,this.password.value).subscribe(value => console.log(value));
+   
+    this.authModel!.email = this.emailHolder.value;
+    this.authModel!.password = this.passwordHolder.value;
+
+   this.httpService.signUp(this.authModel).subscribe(res => console.log("Response"+res));
+    
+  }
+
+  openSnackBar(message: string)
+  {
+    this._snackBar.open(message,'Close', { duration: 5000}); //Snackbar closes quickly need to fix
+  }
 
 }
