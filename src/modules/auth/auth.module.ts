f67be4ba3827f98796126232f9d786fec4b6efd6
Patrick Prakash <patrickprakash8@gmail.com>
2021-12-07 13:32:32 +0530

    Imported the material module to all dependent modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
-
+import { MaterialModule } from '../shared/modules/material/material.module';
 @NgModule({
   declarations: [
     SigninComponent,
     SignupComponent
   ],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
 export class AuthModule {}
--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRouting } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
-
+import { MaterialModule } from '../shared/modules/material/material.module';
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRouting
-  ]
+  declarations: [DashboardComponent],
+  imports: [CommonModule, DashboardRouting, MaterialModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
