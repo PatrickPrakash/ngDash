d97a9e2d47571a0c6792f2497e1f8710ed358491
Patrick Prakash <patrickprakash8@gmail.com>
2021-12-15 18:21:40 +0530

    Implement dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import {
+  MatSlideToggle,
+  MatSlideToggleChange,
+} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private renderer: Renderer2) {}
+  isChecked: boolean = false;
+
+  @ViewChild('parent') parent: any;
+
+  run(event: MatSlideToggleChange): void {
+    this.isChecked = event.checked;
+    if (this.isChecked) {
+      this.renderer.addClass(this.parent.nativeElement, 'dark-theme');
+    } else {
+      this.renderer.removeClass(this.parent.nativeElement, 'dark-theme');
+    }
+  }
+}
