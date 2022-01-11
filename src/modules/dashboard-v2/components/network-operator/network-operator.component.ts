1048be97fadd924c622e62e798f2bfd9d8706c50
Patrick Prakash <patrickprakash8@gmail.com>
2022-01-11 15:29:26 +0530

    Completed the network operator

--- src/modules/dashboard-v2/components/network-operator/network-operator.component.ts
+++ src/modules/dashboard-v2/components/network-operator/network-operator.component.ts
@@ -1,20 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { zoneDetails } from '../../models/zone-details';
-
+import { networkOperator } from '../../models/network-operator-data';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-network-operator',
   templateUrl: './network-operator.component.html',
   styleUrls: ['./network-operator.component.scss'],
 })
-export class NetworkOperatorComponent implements OnInit {
-  constructor() {}
+export class NetworkOperatorComponent {
+  constructor(private fb: FormBuilder) {}
 
-  zoneDetails: zoneDetails[] = [
-    { zoneName: 'A', zonePrice: 10 },
-    { zoneName: 'B', zonePrice: 20 },
-    { zoneName: 'C', zonePrice: 30 },
-  ];
-  ngOnInit(): void {
-    console.log('View initialized');
+  network_operator_data: networkOperator = {
+    network_operator: '',
+    zone_details: [
+      {
+        zoneName: 'A',
+        zonePrice: 10,
+      },
+      {
+        zoneName: 'A',
+        zonePrice: 100,
+      },
+      {
+        zoneName: 'A',
+        zonePrice: 1000,
+      },
+      {
+        zoneName: 'A',
+        zonePrice: 10000,
+      },
+      {
+        zoneName: 'A',
+        zonePrice: 100000,
+      },
+    ],
+  };
+
+  networkOperatorForm = this.fb.group({
+    network_operator: [''],
+    zone_details: this.fb.group({
+      zoneName: [''],
+      zonePrice: [''],
+    }),
+  });
+
+  logData(): void {
+    // Set the data from form
+    this.network_operator_data.network_operator =
+      this.networkOperatorForm.get('network_operator')?.value;
+    // Push the data to the array
+    this.network_operator_data.zone_details.push(
+      this.networkOperatorForm.get('zone_details')?.value
+    );
+  }
+
+  deleteDataItem(data: any): void {
+    this.network_operator_data.zone_details.splice(
+      this.network_operator_data.zone_details.indexOf(data),
+      1
+    );
   }
 }
