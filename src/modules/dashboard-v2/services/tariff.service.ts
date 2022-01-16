a611c3c36a744e791fde8cb99280d996b3068597
Patrick Prakash <patrickprakash8@gmail.com>
2022-01-16 17:12:09 +0530

    Updated the service

--- src/modules/dashboard-v2/services/tariff.service.ts
+++ src/modules/dashboard-v2/services/tariff.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
 import { ReplaySubject, BehaviorSubject, Observable } from 'rxjs';
 import { TariffDetails } from 'src/modules/dashboard-v2/models/tariff-details';
+import { networkOperator } from '../models/network-operator-data';
 @Injectable({
   providedIn: 'root',
 })
 export class TariffService {
-  //   tariffData = new BehaviorSubject<TariffDetails[]>([
-  //     {
-  //       zone: '',
-  //       country: '',
-  //       network_operator: '',
-  //       network_code: 0,
-  //       increment_type: '',
-  //     },
-  //   ]);
   tariffData = new ReplaySubject<TariffDetails[]>();
-  $tariffData = this.tariffData.asObservable();
+  networkData = new ReplaySubject<networkOperator>();
+
+  //$tariffData = this.tariffData.asObservable();
 
   constructor() {}
 
   updateData(tariffData: any) {
     this.tariffData.next(tariffData);
+  }
+
+  getNetworkOpData(): ReplaySubject<networkOperator> {
+    return this.networkData;
+  }
+
+  setNetworkOpData(networkData: networkOperator): void {
+    this.networkData.next(networkData);
   }
 }
